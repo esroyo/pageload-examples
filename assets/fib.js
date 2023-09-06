75829f5dbeab19584e7065832b647c683bd786d0
Carles Escrig Royo <esroyo@gmail.com>
2023-09-06 17:02:22 +0200

    feat: add script async example

---
+++ assets/fib.js
@@ -0,0 +1,12 @@
+performance.mark(`script execution${document.currentScript.dataset.message || ''}`);
+
+function fib(n) {
+  if (n <= 1) return n;
+  return fib(n- 1) + fib(n - 2);
+}
+
+var fibonacciNumber = Number(document.currentScript.dataset.fibonacciNumber);
+
+performance.mark(`start fib(${fibonacciNumber})`);
+fib(fibonacciNumber);
+performance.mark(`end fib(${fibonacciNumber})`);
