1f45fa358865e6f3b1ead8bdcc66f0ed57c05d18
Carles Escrig Royo <esroyo@gmail.com>
2023-09-08 17:39:32 +0200

    fix: make scripts handle being loaded as modules

--- assets/fib.js
+++ assets/fib.js
@@ -1,11 +1,11 @@
-performance.mark(`script execution${document.currentScript.dataset.message || ''}`);
+performance.mark(`script execution${document.currentScript?.dataset?.message || ''}`);
 
 function fib(n) {
   if (n <= 1) return n;
   return fib(n- 1) + fib(n - 2);
 }
 
-var fibonacciNumber = Number(document.currentScript.dataset.fibonacciNumber);
+var fibonacciNumber = Number(document.currentScript?.dataset?.fibonacciNumber) || 10;
 
 performance.mark(`start fib(${fibonacciNumber})`);
 fib(fibonacciNumber);
